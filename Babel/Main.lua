2346d9bdad8124e9e29b56e22c9c2b409babf16a
MelanAster <wzh3078@126.com>
2024-10-29 13:48:29 +0800

    Update 2024.10.29

--- Babel/Main.lua
+++ Babel/Main.lua
@@ -6,7 +6,7 @@ local BB = MABabel
 BB.Name = "Babel"
 BB.Title = "Babel 集束型汉化"
 BB.Author = "SplendidAchievers"
-BB.Version = "2024.10.28"
+BB.Version = "2024.10.29"
 
 --Default/Saved Setting
 BB.Default = {
